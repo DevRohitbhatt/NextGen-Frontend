e1797e3285f7e3ca1a5f9ced43ee857879296f6e
QSR-JoshAllen <josh.allen@qsronline.com>
2024-01-15 15:23:43 -0600

    Initial commit, added a react app with a basic prep chart page.

---
+++ prepchart-frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PrepChart from './pages/PrepChart.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrepChart />
+  </React.StrictMode>,
+)
