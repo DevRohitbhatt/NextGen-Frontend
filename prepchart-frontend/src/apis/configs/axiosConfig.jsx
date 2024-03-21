235a965c2fadb7d20abd9375b398039a45afad10
Jishua-allen <62779308+Jishua-allen@users.noreply.github.com>
2024-03-21 10:25:57 -0600

    Trying to fix the production API requests

--- prepchart-frontend/src/apis/configs/axiosConfig.jsx
+++ prepchart-frontend/src/apis/configs/axiosConfig.jsx
@@ -2,16 +2,15 @@ import axios from "axios";
 
 const api = axios.create({
   withCredentials: true,
-  baseURL: "192.168.192.244:8030/",
+  baseURL: "http://192.168.192.244:8030/",
 });
 
 const errorHandler = (error) => {
-  const statusCode = error.response?.status
-  if (statusCode && statusCode !== 401)
-    console.log(error)
+  const statusCode = error.response?.status;
+  if (statusCode && statusCode !== 401) console.log(error);
 
-  return Promise.reject(error)
-}
+  return Promise.reject(error);
+};
 
 api.interceptors.response.use(undefined, (error) => {
   return errorHandler(error);
