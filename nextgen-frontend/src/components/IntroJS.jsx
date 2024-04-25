4c7db938cff19d66873de9d5701ac6cb65cf96c2
QSR-JoshAllen <josh.allen@qsronline.com>
2024-04-25 15:36:01 -0500

    Adding an intro js component (unfinished)

---
+++ nextgen-frontend/src/components/IntroJS.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Steps, Hints } from "intro.js-react";
+import "intro.js/introjs.css";
+
+export default function IntroJS(introJS, setIntroJS) {
+    //Destructure introJS into steps, enabled, so on
+
+    const handleIntroStart = () => {
+        setIntroJS({ ...introJS, stepsEnabled: true });
+      };
+
+    return (
+        <Steps
+            enabled={introJS.enabled}
+            steps={introJS.steps}
+            initialStep={introJS.initialStep}
+            onExit={() => {
+                setIntroJS({ ...introJS, enabled: false });
+            }}
+        />
+    )
