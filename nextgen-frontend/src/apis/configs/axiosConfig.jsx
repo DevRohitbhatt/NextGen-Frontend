06e7f1f212476ef49155046f5ee79e2c1a7b3653
QSR-JoshAllen <josh.allen@qsronline.com>
2024-05-03 08:56:00 -0500

    Updated the baseURL to the api on H107

--- nextgen-frontend/src/apis/configs/axiosConfig.jsx
+++ nextgen-frontend/src/apis/configs/axiosConfig.jsx
@@ -8,7 +8,7 @@ const api = axios.create({
     'Allow-Control-Allow-Origin': '*',
   },
   'Content-Type': 'application/json',
-  baseURL: "https://192.168.192.244/NextGenAPI/",
+  baseURL: "https://www7.qsronline.com/NextGenAPI/",
 });
 
 const errorHandler = (error) => {
