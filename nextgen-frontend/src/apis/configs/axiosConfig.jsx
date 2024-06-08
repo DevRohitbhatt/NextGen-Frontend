e0576f65da9efa9176c8290482407ed7ce5b0348
QSR-JoshAllen <josh.allen@qsronline.com>
2024-06-08 08:05:10 -0500

    Added the axios config back into the repo

---
+++ nextgen-frontend/src/apis/configs/axiosConfig.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://192.168.192.244:8030/";
+
+const api = axios.create({
+  withCredentials: true,
+  headers: {
+    'Allow-Control-Allow-Origin': '*',
+  },
+  'Content-Type': 'application/json',
+  baseURL: "https://www1.qsronline.com/QSROnlineGateway/",
+});
+
+const errorHandler = (error) => {
+  const statusCode = error.response?.status;
+  if (statusCode && statusCode !== 401) console.log(error);
+
+  return Promise.reject(error);
+};
+
+api.interceptors.response.use(undefined, (error) => {
+  return errorHandler(error);
+});
+
+api.interceptors.request.use((config) => {
+  //TODO: get accessToken from authentication server
+  // then use localStorage.getItem("accessToken");
+  const accessToken = "tempToken";
+
+  config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+export default api;
