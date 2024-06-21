36329d14c95d5a97dd2e791abfc7f5454598ea96
QSR-JoshAllen <josh.allen@qsronline.com>
2024-06-21 13:18:37 -0500

    Changing the baseURL for the testing server

--- nextgen-frontend/src/apis/configs/axiosConfig.jsx
+++ nextgen-frontend/src/apis/configs/axiosConfig.jsx
@@ -8,7 +8,7 @@ const api = axios.create({
     'Allow-Control-Allow-Origin': '*',
   },
   'Content-Type': 'application/json',
-  baseURL: "https://localhost:7278/",
+  baseURL: "https://d101.qsronline.com/QSROnlineGateway/",
 });
 
 const errorHandler = (error) => {
