c64e5dbc91a0ff26372103b5e604e768b103f1c8
Rohit Bhatt <devrohitbhatt@gmail.com>
2024-10-17 16:28:45 +0530

    Refactor: Update run button styling in LaborCICO.jsx

--- nextgen-frontend/src/pages/labor/LaborCICO.jsx
+++ nextgen-frontend/src/pages/labor/LaborCICO.jsx
@@ -564,7 +564,7 @@ const LaborCICO = () => {
 							onClick={() => setShowDateModal(true)}
 						/>
 						<div className='run-button' onClick={handleRunClick}>
-							<div className='py-3 text-lg font-bold text-center capitalize border-2 border-solid cursor-pointer px-14 hover:border-primary hover:text-white hover:bg-primary text-nowrap rounded-3xl mt-7'>
+							<div className='py-3 text-lg font-bold text-center capitalize border-2 border-solid cursor-pointer px-14 hover:border-[var(--tw-primary)] hover:text-white hover:bg-[var(--tw-primary)] text-nowrap rounded-3xl mt-7'>
 								Run
 							</div>
 						</div>
