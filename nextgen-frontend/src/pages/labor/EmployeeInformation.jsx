b0f31df029079dd3eb9cce05ef7a4bbf3c6e4d21
Rohit Bhatt <devrohitbhatt@gmail.com>
2024-11-23 12:07:02 +0530

    fixed report not bring up the data on intial loads

--- nextgen-frontend/src/pages/labor/EmployeeInformation.jsx
+++ nextgen-frontend/src/pages/labor/EmployeeInformation.jsx
@@ -287,6 +287,12 @@ const EmployeeInformation = () => {
 		}
 	}, [defaultUnitID, defaultUnitName]);
 
+	useEffect(() => {
+		if (selectedUnit) {
+			fetchEmployeeInformation(companyID, alignmentID, selectedUnit);
+		}
+	}, [selectedUnit]);
+
 	// Fetching Employee Information
 	const fetchEmployeeInformation = async (companyId, alignmentId, selectedUnit) => {
 		try {
@@ -337,9 +343,6 @@ const EmployeeInformation = () => {
 		setSelectedUnitName(unitName);
 		setSelectedUnit(unitID);
 		setShowUnitModal(false);
-		setIsLoading(true);
-		await fetchEmployeeInformation(companyID, alignmentID, unitID);
-		setIsLoading(false);
 	};
 
 	// function	to handle the view change
