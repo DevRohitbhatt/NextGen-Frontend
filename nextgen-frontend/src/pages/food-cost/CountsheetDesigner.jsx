e8171f219984b14715335760b586ec1a6a4a98da
Rohit Bhatt <devrohitbhatt@gmail.com>
2024-12-02 12:09:20 +0530

    Remove console log from CountsheetDesigner useEffect for cleaner code

--- nextgen-frontend/src/pages/food-cost/CountsheetDesigner.jsx
+++ nextgen-frontend/src/pages/food-cost/CountsheetDesigner.jsx
@@ -102,7 +102,6 @@ const CountsheetDesigner = () => {
 	useEffect(() => {
 		setCountsheet(location.state.countsheet);
 		setSelectedFromDate(new Date(location.state.countsheet?.dateTime));
-		console.log(location.state);
 
 		fetchCountsheetDetails();
 	}, []);
