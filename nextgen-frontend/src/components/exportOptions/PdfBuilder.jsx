1ca7d82e3e7c21436ed8c595bfc17eab04c2d37a
Pawandeep <179115625+stk-pawandeep@users.noreply.github.com>
2024-12-04 16:11:27 +0530

    Fix : pdf color issue.

--- nextgen-frontend/src/components/exportOptions/PdfBuilder.jsx
+++ nextgen-frontend/src/components/exportOptions/PdfBuilder.jsx
@@ -1,127 +1,163 @@
-import pdfMake from 'pdfmake/build/pdfmake';
+import pdfMake from "pdfmake/build/pdfmake";
 // import pdfFonts from 'pdfmake/build/vfs_fonts';
 // pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 pdfMake.fonts = {
-	Roboto: {
-		normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
-		bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
-		italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
-		bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf',
-	},
+  Roboto: {
+    normal:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+    bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+    italics:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+    bolditalics:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+  },
+};
+
+const rgbToHex = (rgb) => {
+  // If RGB is an array, extract r, g, b values
+  if (Array.isArray(rgb)) {
+    const [r, g, b] = rgb;
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
+
+  // If the input is a string like 'rgb(255, 0, 0)'
+  const match = rgb.match(
+    /^rgb(?:a)?\((\d+), (\d+), (\d+)(?:, (\d+(\.\d+)?))?\)$/
+  );
+  if (match) {
+    const [r, g, b] = [
+      parseInt(match[1]),
+      parseInt(match[2]),
+      parseInt(match[3]),
+    ];
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
+
+  return rgb; // If not in valid RGB or RGBA format, return the original string
+};
+
+// Helper function to convert a number to a 2-digit hex string
+const toHex = (num) => {
+  const hex = num.toString(16);
+  return hex.length === 1 ? "0" + hex : hex;
 };
 
 const createTable = (tableInfo) => {
-	const title = { text: '', style: 'tableTitle' };
-	if (tableInfo.title) title.text = tableInfo.title;
-	const table = {
-		table: {
-			headerRows: 1,
-			widths: tableInfo.widths,
-			body: [
-				[
-					...tableInfo.data.columnHeaders.map((header) => ({
-						text: header,
-						style: 'tableHeader',
-					})),
-				],
-				...tableInfo.data.rows.map((row) =>
-					row.map((cell, index) => ({
-						text: getCellValue(cell, tableInfo.dataTypes ? tableInfo.dataTypes[index] : ''),
-						style: 'tableCell',
-					}))
-				),
-			],
-		},
-		layout: {
-			fillColor: function (rowIndex, node, columnIndex) {
-				return rowIndex % 2 === 0 ? '#E0E0E0' : null;
-			},
-		},
-	};
+  const title = { text: "", style: "tableTitle" };
+  if (tableInfo.title) title.text = tableInfo.title;
+  const table = {
+    table: {
+      headerRows: 1,
+      widths: tableInfo.widths,
+      body: [
+        [
+          ...tableInfo.data.columnHeaders.map((header) => ({
+            text: header,
+            style: "tableHeader",
+          })),
+        ],
+        ...tableInfo.data.rows.map((row) =>
+          row.map((cell, index) => ({
+            text: getCellValue(
+              cell,
+              tableInfo.dataTypes ? tableInfo.dataTypes[index] : ""
+            ),
+            style: "tableCell",
+            fillColor: cell.color ? rgbToHex(cell.color) : null,
+          }))
+        ),
+      ],
+    },
+    layout: {
+      fillColor: function (rowIndex, node, columnIndex) {
+        return rowIndex % 2 === 0 ? "#E0E0E0" : null;
+      },
+    },
+  };
 
-	if (title.text === '') return { table };
-	else return { title, table };
+  if (title.text === "") return { table };
+  else return { title, table };
 };
 
 const getCellValue = (cell, dataType) => {
-	if (Array.isArray(cell.value)) {
-		return cell.value.find((option) => option.isSelected).option;
-	} else if (dataType === 'currency') {
-		return cell.value
-			? cell.value.toLocaleString('en-US', {
-					style: 'currency',
-					currency: 'USD',
-			  })
-			: '';
-	} else if (dataType === 'currency rounded') {
-		const roundedValue = Math.round(cell.value);
-		return '$' + roundedValue.toLocaleString('en-US');
-	} else if (dataType === 'percent') {
-		return `${cell.value}%`;
-	} else {
-		return cell.value !== 0 ? cell.value : '';
-	}
+  if (Array.isArray(cell.value)) {
+    return cell.value.find((option) => option.isSelected).option;
+  } else if (dataType === "currency") {
+    return cell.value
+      ? cell.value.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })
+      : "";
+  } else if (dataType === "currency rounded") {
+    const roundedValue = Math.round(cell.value);
+    return "$" + roundedValue.toLocaleString("en-US");
+  } else if (dataType === "percent") {
+    return `${cell.value}%`;
+  } else {
+    return cell.value !== 0 ? cell.value : "";
+  }
 };
 
 export default function PdfBuilder(data) {
-	const content = [];
-	let columns = [];
-	content.push({ text: data.title, style: 'header' });
-	if (data.subHeaders) {
-		data.subHeaders.map((subHeader) => {
-			content.push({ text: subHeader, style: 'subheader' });
-		});
-	}
-	data.body.map((section) => {
-		if (section.type !== 'table/Column' && columns.length > 0) {
-			content.push({ columns: columns });
-			columns = [];
-		}
-		if (section.type === 'table') {
-			const { title, table } = createTable(section);
-			if (title) content.push(title);
-			content.push(table);
-		} else if (section.type === 'table/Column') {
-			const { title, table } = createTable(section);
-			if (title) content.push(title);
-			columns.push(table);
-		}
-	});
+  const content = [];
+  let columns = [];
+  content.push({ text: data.title, style: "header" });
+  if (data.subHeaders) {
+    data.subHeaders.map((subHeader) => {
+      content.push({ text: subHeader, style: "subheader" });
+    });
+  }
+  data.body.map((section) => {
+    if (section.type !== "table/Column" && columns.length > 0) {
+      content.push({ columns: columns });
+      columns = [];
+    }
+    if (section.type === "table") {
+      const { title, table } = createTable(section);
+      if (title) content.push(title);
+      content.push(table);
+    } else if (section.type === "table/Column") {
+      const { title, table } = createTable(section);
+      if (title) content.push(title);
+      columns.push(table);
+    }
+  });
 
-	const docDefinition = {
-		pageOrientation: data.pageOrientation || 'portrait',
-		content: content,
-		...(data.exportType === 'pdf' && { pageMargins: [20, 20, 20, 20] }),
-		styles: {
-			header: {
-				fontSize: 16,
-				bold: true,
-			},
-			subheader: {
-				fontSize: 12,
-				bold: true,
-				margin: [0, 5, 0, 0],
-			},
-			tableTitle: {
-				fontSize: 12,
-				bold: true,
-				margin: [0, 10, 0, 5],
-			},
-			tableHeader: {
-				bold: true,
-				fontSize: 9,
-				margin: [0, 2, 0, 2],
-			},
-			tableCell: {
-				fontSize: 9,
-			},
-		},
-	};
+  const docDefinition = {
+    pageOrientation: data.pageOrientation || "portrait",
+    content: content,
+    ...(data.exportType === "pdf" && { pageMargins: [20, 20, 20, 20] }),
+    styles: {
+      header: {
+        fontSize: 16,
+        bold: true,
+      },
+      subheader: {
+        fontSize: 12,
+        bold: true,
+        margin: [0, 5, 0, 0],
+      },
+      tableTitle: {
+        fontSize: 12,
+        bold: true,
+        margin: [0, 10, 0, 5],
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 9,
+        margin: [0, 2, 0, 2],
+      },
+      tableCell: {
+        fontSize: 9,
+      },
+    },
+  };
 
-	if (data.exportType === 'pdf') {
-		pdfMake.createPdf(docDefinition).open();
-	} else if (data.exportType === 'print') {
-		pdfMake.createPdf(docDefinition).print();
-	}
+  if (data.exportType === "pdf") {
+    pdfMake.createPdf(docDefinition).open();
+  } else if (data.exportType === "print") {
+    pdfMake.createPdf(docDefinition).print();
+  }
 }
