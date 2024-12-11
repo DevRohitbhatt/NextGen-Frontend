548f638a8c46d5233ee531edaa44ea81dacd54ae
Rohit Bhatt <devrohitbhatt@gmail.com>
2024-12-11 07:49:36 +0530

    Add comment functionality to CountsheetDesigner; enhance total amount display in Invoices; clean up unused search parameters in PurchaseAnalysis

--- nextgen-frontend/src/pages/food-cost/Invoices.jsx
+++ nextgen-frontend/src/pages/food-cost/Invoices.jsx
@@ -154,6 +154,18 @@ const Invoices = () => {
 			columnHelper.accessor('totalAmountIncludingTax', {
 				id: 'totalAmountIncludingTax',
 				header: 'Total',
+				cell: ({ getValue }) => {
+					const value = getValue();
+					return value < 0
+						? `-$${Math.abs(parseFloat(value)).toLocaleString('en-US', {
+								minimumFractionDigits: 2,
+								maximumFractionDigits: 2,
+						  })}`
+						: `$${parseFloat(value).toLocaleString('en-US', {
+								minimumFractionDigits: 2,
+								maximumFractionDigits: 2,
+						  })}`;
+				},
 				dataType: 'number',
 				size: 60,
 			}),
