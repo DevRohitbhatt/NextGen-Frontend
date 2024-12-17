50a2c0837b48e65f13908d49336aeb73721fa444
Pawandeep <179115625+stk-pawandeep@users.noreply.github.com>
2024-12-17 16:58:20 +0530

    Ui fixes.

--- nextgen-frontend/src/pages/sales/BrumitDWP.jsx
+++ nextgen-frontend/src/pages/sales/BrumitDWP.jsx
@@ -347,14 +347,14 @@ const BrumitDWP = () => {
 	const Table = <TableHOC columns={columns} data={brumitDWPData} expandCollapseButtons={true} />;
 
 	return (
-		<div className='w-[85%] mx-auto'>
+		<div className='w-[98%] mx-auto'>
 			<Steps
 				enabled={introSteps.stepsEnabled}
 				steps={introSteps.steps}
 				initialStep={introSteps.initialStep}
 				onExit={() => setIntroSteps({ ...introSteps, stepsEnabled: false })}
 			/>
-			<h2 className='my-4 text-2xl leading-tight text-left pageTitle'>Brumit DWP</h2>
+			<h2 className='my-4 text-[20px] leading-tight text-left pageTitle'>Brumit DWP</h2>
 			<header className='optionsBar flex justify-between items-center mb-2 rounded-2xl p-4 shadow-[0px_3px_20px_-10px_rgba(0,_0,_0,_0.5)]'>
 				<div className='flex items-center'>
 					<UnitSelector
@@ -381,7 +381,7 @@ const BrumitDWP = () => {
 						/>
 					</div>
 					<div className='run-button' onClick={fetchBrumitDWP}>
-						<div className='py-3 ml-3 text-lg font-bold text-center capitalize border-2 border-solid cursor-pointer px-14 hover:border-[var(--tw-primary)] hover:text-white hover:bg-[var(--tw-primary)] text-nowrap rounded-3xl mt-7'>
+						<div className='py-3 ml-3 text-[16px] font-bold text-center capitalize border-2 border-solid cursor-pointer px-14 hover:border-[var(--tw-primary)] hover:text-white hover:bg-[var(--tw-primary)] text-nowrap rounded-3xl mt-7'>
 							Run
 						</div>
 					</div>
