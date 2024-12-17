d61552be8ddae9ee6512479c74199b43a9f1e0c1
Pawandeep <179115625+stk-pawandeep@users.noreply.github.com>
2024-12-17 18:55:35 +0530

    fix accrording to app 3 ui

--- nextgen-frontend/src/pages/sales/BrumitDWP.jsx
+++ nextgen-frontend/src/pages/sales/BrumitDWP.jsx
@@ -354,7 +354,7 @@ const BrumitDWP = () => {
 				initialStep={introSteps.initialStep}
 				onExit={() => setIntroSteps({ ...introSteps, stepsEnabled: false })}
 			/>
-			<h2 className='my-4 text-[20px] leading-tight text-left pageTitle'>Brumit DWP</h2>
+			<h2 className='my-2 text-[18px] leading-tight text-left pageTitle'>Brumit DWP</h2>
 			<header className='optionsBar flex justify-between items-center mb-2 rounded-2xl p-4 shadow-[0px_3px_20px_-10px_rgba(0,_0,_0,_0.5)]'>
 				<div className='flex items-center'>
 					<UnitSelector
@@ -381,7 +381,7 @@ const BrumitDWP = () => {
 						/>
 					</div>
 					<div className='run-button' onClick={fetchBrumitDWP}>
-						<div className='py-3 ml-3 text-[16px] font-bold text-center capitalize border-2 border-solid cursor-pointer px-14 hover:border-[var(--tw-primary)] hover:text-white hover:bg-[var(--tw-primary)] text-nowrap rounded-3xl mt-7'>
+						<div className='py-2 ml-3 text-[14px] font-bold text-center capitalize border-2 border-solid cursor-pointer px-14 hover:border-[var(--tw-primary)] hover:text-white hover:bg-[var(--tw-primary)] text-nowrap rounded-3xl mt-7'>
 							Run
 						</div>
 					</div>
