917ff5aa64e5490caf275f9cf1ec62899ae24ccc
Rohit Bhatt <devrohitbhatt@gmail.com>
2025-01-09 16:44:48 +0530

    Refactor food cost components to simplify item lookup and calculation logic

--- nextgen-frontend/src/pages/food-cost/VarianceFoodCost.jsx
+++ nextgen-frontend/src/pages/food-cost/VarianceFoodCost.jsx
@@ -301,7 +301,7 @@ const VarianceFoodCost = () => {
 							subrow.subRows.reduce((subAcc, subSubrow) => {
 								if (subSubrow.getCanExpand()) {
 									const item = checkedItems.find(
-										(item) => item.name.split('/')[1] === subSubrow.original.subDepartment
+										(item) => item.name.split(/\/(.+)/)[1] === subSubrow.original.subDepartment
 									);
 									return (
 										subAcc +
@@ -315,15 +315,7 @@ const VarianceFoodCost = () => {
 										)
 									);
 								} else {
-									const item = checkedItems.find(
-										(item) => item.name.split('/')[1] === subrow.original.subDepartment
-									);
-									return (
-										subAcc +
-										(item?.includeInGrandTotal && subSubrow.original[field]
-											? Number(subSubrow.original[field])
-											: 0)
-									);
+									return subAcc + (subSubrow.original[field] ? Number(subSubrow.original[field]) : 0);
 								}
 							}, 0)
 						);
